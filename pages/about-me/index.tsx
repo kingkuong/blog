77d85c8d155024503240051c74d1829f9b9289d8
Cuong <33468992+cuongtranx@users.noreply.github.com>
2022-12-21 13:59:05 -0500

    Initialize project (#1)

---
+++ pages/about-me/index.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function AboutMePage() {
+  return (
+    <>
+      <Link href="/">Back to home</Link>
+      <h1>About Me Page</h1>
+    </>
+  );
+}
