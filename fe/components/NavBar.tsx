e9b74f71f6b707d77a82a403aefb0f61e5b98933
Cuong <33468992+cuongtranx@users.noreply.github.com>
2022-12-23 15:50:16 -0500

    Add styling (#4)
    Added: 
    * MUI 5 & Emotion
    * Basic Navbar

---
+++ fe/components/NavBar.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Link from "next/link";
+
+import MenuIcon from "@mui/icons-material/Menu";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export const Navbar = () => {
+  const pages = [
+    {
+      title: "Posts",
+      href: "/posts",
+    },
+    {
+      title: "About Me",
+      href: "/about-me",
+    },
+  ];
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page.title}
+                sx={{
+                  my: 2,
+                  display: "block",
+                }}
+              >
+                <Link href={page.href} passHref>
+                  <Typography
+                    variant="subtitle1"
+                    color="white"
+                    sx={{ textDecoration: "none" }}
+                  >
+                    {page.title.toUpperCase()}
+                  </Typography>
+                </Link>
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
